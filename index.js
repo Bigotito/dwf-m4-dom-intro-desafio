b2204f97e31cd531702d42717824e9bbe46d6ed1
Gabriel González <mgabrieltbf@gmail.com>
2023-08-08 15:15:18 -0300

    js modificado

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  let elementosDeLista = document.querySelectorAll('.lista li');
+
+  console.log(elementosDeLista);
+
+  let listaSup = document.querySelector('.lista');
+  elementosDeLista.forEach((elemento) => {
+    listaSup.removeChild(elemento);
+  });
+
+  // for(let i=0;i<elementosDeLista.length;i++){
+  //   document.querySelector('.lista').removeChild(elementosDeLista[i]);
+  // }
+
+  let nuevoLi;
+  cosasQueAprendimos.forEach((temaActual) => {
+    nuevoLi = document.createElement("li");
+    nuevoLi.className = temaActual.class;
+    nuevoLi.innerText = temaActual.tema;
+    listaSup.appendChild(nuevoLi);
+  })
+  
+}
 
 main();
